d496ac390ae642e739853084dc122e1f0f818dbc
Davyd McColl <davydm@gmail.com>
2018-07-25 11:00:51 +0200

    fix: typo

--- src/trello-sidebar-hack.js
+++ src/trello-sidebar-hack.js
@@ -1,16 +1,17 @@
+"use strict";
 var addToSidebarFunc = require('./modules/add-to-sidebar');
 
-function PullRequestsHack() {
+function TrelloSidebarHack() {
     this.init();
 }
 
-ExpandCommentsHack.prototype = {
+TrelloSidebarHack.prototype = {
     destroy: function(){
     },
     init: function(){
         var comments = Array.from(document.getElementsByClassName('d-block comment-body markdown-body  js-comment-body'));
         var trelloComments = comments.filter(x => x.innerHTML.indexOf("trello") !== -1);
-        
+
         if(trelloComments.length !== 1) {
             console.log(`WARNING: ${comments.length} comments detected. ${trelloComments.length} trello items detected.`);
             return;
@@ -21,5 +22,5 @@ ExpandCommentsHack.prototype = {
     }
 };
 
-PullRequestsHack.urlMatch = /.*\/pull\/[\d]+\/?$/;
-module.exports = PullRequestsHack;
+TrelloSidebarHack.urlMatch = /.*\/pull\/[\d]+\/?$/;
+module.exports = TrelloSidebarHack;
