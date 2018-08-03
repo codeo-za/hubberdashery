0b23403e5c4d6b4413c7fc44f8da1cb396d41ca1
Davyd McColl <davydm@gmail.com>
2018-08-03 14:45:46 +0200

    chore: rebuild without notificatins view hack

--- src/tampermonkey-header.js
+++ src/tampermonkey-header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hubberdashery
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Some hackdashery for your githubz.
 // @author       Space Monkey Extraordinaire!
 // @match        https://github.com/*
